8e237c4de524251b3564843ec204da2854bb2890
GGerake <gabrielgerake@outlook.com>
2025-07-16 14:37:44 -0300

    adicionar estilo

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Servir arquivos estáticos da raiz do projeto (como index.html)
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
